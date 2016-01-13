9ce61d5426207a95d0c57acae47d5ab286176909
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-01-13 14:33:38 -0700

    caught bug exception

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ var userFirstName, userLastName, userFBid;
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "https://http://galaxybomber.herokuapp.com/auth/facebook/callback",
+    // callbackURL: "https://localhost:3000/auth/facebook/callback",
+    callbackURL: "http://galaxybomber.herokuapp.com/auth/facebook/callback",
     enableProof: false,
     profileFields: ['id', 'displayName', 'link', 'photos', 'email']
   },
