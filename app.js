d68f4e76c8b88195dab5576e2013a219f8f817b4
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-09-01 10:24:21 -0600

    update with correct cb

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ var userFirstName, userLastName, userFBid;
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-    // callbackURL: "https://galaxybomber.herokuapp.com/auth/facebook/callback",
+    // callbackURL: "http://localhost:3000/auth/facebook/callback",
+    callbackURL: "https://galaxybomber.herokuapp.com/auth/facebook/callback",
     enableProof: false,
     profileFields: ['id', 'displayName', 'link', 'photos', 'email']
   },
